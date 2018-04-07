628a0304802e6b745d64b6ab2b3ba9e74fa2f421
Alex Yang <a.yang2@share.epsb.ca>
2018-04-07 15:48:33 +0000

    Added response for answer

--- NumberGame.java
+++ NumberGame.java
@@ -9,12 +9,15 @@ public class NumberGame
 		Scanner kbReader = new Scanner(System.in);
 		int num = (int)Math.floor(Math.random() * 100); //ramdom number between 1 and 100
 		
-		System.out.println(The computer has the secret number...);
-		System.out.print(Enter your guess (1-100));
+		System.out.println(num);
+		System.out.println("The computer has the secret number...");
+		System.out.print("Enter your guess (1-100)");
 		
+		int guesscount = 0;
 		int guess = kbReader.nextInt();
-		
-		
-		
+		if (guess == num)
+		{
+		System.out.println("Correct!");
+		}
 	}
 }
