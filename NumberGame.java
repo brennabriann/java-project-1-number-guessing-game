365d8e10075b339d5b2778a47a5f9273d02de75c
Alex Yang <a.yang2@share.epsb.ca>
2018-04-07 16:21:42 +0000

    Cleaned code

--- NumberGame.java
+++ NumberGame.java
@@ -4,12 +4,12 @@ import java.io.*;
 import java.util.*;
 public class NumberGame
 {
+
 	public static void main(String args[])
 	{
 		Scanner kbReader = new Scanner(System.in);
 		int num = (int)Math.floor(Math.random() * 100); //ramdom number between 1 and 100
 		
-		System.out.println(num);
 		System.out.println("The computer has the secret number...");
 		System.out.print("Enter your guess (1-100)");
 		
@@ -32,18 +32,16 @@ public class NumberGame
 			System.out.print("Enter your guess (1-100):");
 			guess = kbReader.nextInt();
 			}
-			else 
+			else
 			{
 			System.out.print("Bad guess. Please re-enter:");
 			guess = kbReader.nextInt();
 			}
 		}
-		
+
 		//code runs if user is correct
 		guesscount++;
 		System.out.println("Correct!");
 		System.out.println("You used " + guesscount + " guesses.");
-		                        
-		
 	}
 }
