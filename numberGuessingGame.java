0ae25582abb7228636fb31251f417e59caef8d31
Mostafa Rizk <m.rizk.student@gmail.com>
2018-04-29 12:22:04 -0600

    Declared and initialized variables

--- numberGuessingGame.java
+++ numberGuessingGame.java
@@ -2,6 +2,9 @@ import java.io.*;
 import java.util.*;
 public class numberGuessingGame {
    public static void main(String args[]) {
-      
+      Scanner kbReader = new Scanner(System.in);
+      int secretNum = (int)Math.floor(Math.random() * 101); //Generates a random number between 0 and 100
+      int userGuess, numGuesses = 0;
+      System.out.println("The computer has the secret number...");
    }
 }
