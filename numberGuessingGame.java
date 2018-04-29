dc49a2659e643681cb97b660830a8375a56eac06
Mostafa Rizk <m.rizk.student@gmail.com>
2018-04-29 12:29:12 -0600

    Created skeleton code for loop

--- numberGuessingGame.java
+++ numberGuessingGame.java
@@ -6,5 +6,20 @@ public class numberGuessingGame {
       int secretNum = (int)Math.floor(Math.random() * 101); //Generates a random number between 0 and 100
       int userGuess, numGuesses = 0;
       System.out.println("The computer has the secret number...");
+      do {
+         System.out.print("Enter your guess (1-100): ");
+         userGuess = kbReader.nextInt();
+         numGuesses++;
+         if (userGuess > secretNum) {
+         
+         }
+         else if userGuess < secretNum) {
+         
+         }
+         else {
+         
+         }
+      }
+      while (userGuess != secretNum);
    }
 }
